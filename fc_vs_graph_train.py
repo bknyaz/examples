012debae8e1668e4e5128c4dc04520c8a68bc9a3
Boris Knyazev <bknyazev@uoguelph.ca>
2019-07-18 17:26:06 -0400

    fc vs graph

---
+++ fc_vs_graph_train.py
@@ -0,0 +1,172 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import numpy as np
+from scipy.spatial.distance import cdist
+
+
+class BorisNet(nn.Module):
+    def __init__(self):
+        super(BorisNet, self).__init__()
+        self.fc = nn.Linear(784, 10, bias=False)
+
+    def forward(self, x):
+        return self.fc(x.view(x.size(0), -1))
+
+
+class BorisGraphNet(nn.Module):
+    def __init__(self, img_size=28, pred_edge=False):
+        super(BorisGraphNet, self).__init__()
+        self.pred_edge = pred_edge
+        N = img_size ** 2
+        self.fc = nn.Linear(N, 10, bias=False)
+        if pred_edge:
+            col, row = np.meshgrid(np.arange(img_size), np.arange(img_size))
+            coord = np.stack((col, row), axis=2).reshape(-1, 2) / img_size - 0.5
+            coord = torch.from_numpy(coord).float()  # 784,2
+            coord = torch.cat((coord.unsqueeze(0).repeat(N, 1,  1),
+                                    coord.unsqueeze(1).repeat(1, N, 1)), dim=2)
+            self.pred_edge_fc = nn.Sequential(nn.Linear(4, 32),
+                                              nn.ReLU(),
+                                              nn.Linear(32, 1),
+                                              nn.Tanh())
+            self.register_buffer('coord', coord)
+        else:
+            # precompute adjacency matrix before training
+            A = self.precompute_adjacency_images(img_size)
+            self.register_buffer('A', A)
+
+
+    @staticmethod
+    def precompute_adjacency_images(img_size):
+        col, row = np.meshgrid(np.arange(img_size), np.arange(img_size))
+        coord = np.stack((col, row), axis=2).reshape(-1, 2) / img_size
+        dist = cdist(coord, coord)
+        sigma = 0.05 * np.pi
+        A = np.exp(- dist / sigma ** 2)
+        A[A < 0.01] = 0
+        A = torch.from_numpy(A).float()
+
+        # Normalization as per (Kipf & Welling, ICLR 2017)
+        D = A.sum(1)  # nodes degree (N,)
+        D_hat = (D + 1e-5) ** (-0.5)
+        A_hat = D_hat.view(-1, 1) * A * D_hat.view(1, -1)  # N,N
+
+        # Some additional trick I found to be useful
+        A_hat[A_hat > 0.0001] = A_hat[A_hat > 0.0001] - 0.2
+
+        print(A_hat[:10, :10])
+        return A_hat
+
+    def forward(self, x):
+        B = x.size(0)
+        if self.pred_edge:
+            self.A = self.pred_edge_fc(self.coord).squeeze()
+        avg_neighbor_features = (torch.bmm(self.A.unsqueeze(0).expand(B, -1, -1),
+                                 x.view(B, -1, 1)).view(B, -1))
+        return self.fc(avg_neighbor_features)
+
+
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.cross_entropy(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+
+def test(args, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.cross_entropy(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        '\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--model', type=str, default='graph', choices=['fc', 'graph'],
+                        help='model to use for training (default: fc)')
+    parser.add_argument('--batch-size', type=int, default=64,
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000,
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10,
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.001,
+                        help='learning rate (default: 0.001)')
+    parser.add_argument('--pred_edge', action='store_true', default=False,
+                        help='predict edges instead of using predefined ones')
+    parser.add_argument('--seed', type=int, default=1,
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=200,
+                        help='how many batches to wait before logging training status')
+
+    args = parser.parse_args()
+    use_cuda = True
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('../data', train=False, transform=transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])),
+        batch_size=args.test_batch_size, shuffle=False, **kwargs)
+
+    if args.model == 'fc':
+        assert not args.pred_edge, "this flag is meant for graphs"
+        model = BorisNet()
+    elif args.model == 'graph':
+        model = BorisGraphNet(pred_edge=args.pred_edge)
+    else:
+        raise NotImplementedError(args.model)
+    model.to(device)
+    print(model)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr)
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        test(args, model, device, test_loader)
+
+
+if __name__ == '__main__':
+    main()
+    # Examples:
+    # python mnist_fc.py --model fc
+    # python mnist_fc.py
+    # python mnist_fc.py --pred_edge
